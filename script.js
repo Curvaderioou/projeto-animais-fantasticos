6c2d9a8b4d689acc2c5f84f2d3ac5f5d23cd704d
Curvaderioou <felipedegenerportella@yahoo.com>
2021-06-17 21:09:37 -0300

    exercicios da data-atribute

---
+++ script.js
@@ -0,0 +1,10 @@
+// add atribuo data-anime="show-down" e data-anime="show-right" a todas as sections com descrição de animais
+
+
+// utilizando esses atributos, adicione a class show-down e rigth a sua respectiva section, para aparecer a animação tab
+
+
+// no css faça com que o show-down anime de cima para baixo e o show rigth continue com a mesma animação da esquerda para direita
+
+
+// substitua todas as classes js- por data atributes
